f3505e6140a07740dbbc60db5c5208f1c170be78
PopovicMilica <popovic.v.milica@gmail.com>
2020-07-14 15:16:31 +0200

    Update setup.py
    'Updated setup.py'

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
-long_description = '''Software for estimating treatment
-                      effects and propensity scores.'''
-	  
-setup(name='causal_nets',
+from setuptools import find_packages
+
+description = '''Software for estimation of heterogeneous treatment effects
+                 and propensity scores.'''
+  
+setup(
+      name='causal_nets',
       version='0.0.1',
-      long_description=long_description,
+      description=description,
       author='Milica Popovic',
       author_email='popovic.v.milica@gmail.com',
-      packages=['causal_nets'],
+      keywords=['deep learning', 'treatment effects', 'causal inference'],
       install_requires=['numpy==1.16.4',
-                        'tensorflow>=1.14.0'])
+                        'tensorflow==2.1.0'],
+      extras_require={
+        'tests': ['scikit-learn']},
+      packages=find_packages(exclude=('tests',))
+)
